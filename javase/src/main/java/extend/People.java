8e4a487615ef1c7e30e4d0b5bbf688cc1df58ec0
chenmin <chen.min55@iwhalecloud.com>
2020-08-19 19:52:09 +0800

    fix

---
+++ javase/src/main/java/extend/People.java
@@ -0,0 +1,28 @@
+package extend;
+
+/**
+ * @program: javaNotes
+ * @description:
+ * @author: cm
+ * @create: 2020-08-19 19:35
+ **/
+public class People {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
