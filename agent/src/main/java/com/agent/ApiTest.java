1241109acc830635f6fe0b9af592a145c03d11a9
chenmget <502298798@qq.com>
2020-09-16 23:01:15 +0800

    fix

--- agent/src/main/java/com/agent/ApiTest.java
+++ agent/src/main/java/com/agent/ApiTest.java
@@ -11,8 +11,30 @@ public class ApiTest {
         System.out.println("hello word");
         ApiTest apiTest = new ApiTest();
         apiTest.echoHi();
+
+
+//        //线程一
+//        new Thread(() -> new ApiTest().http_lt1()).start();
+//
+//        //线程二
+//        new Thread(() -> { new ApiTest().http_lt1(); }).start();
     }
     private void echoHi(){
         System.out.println("hi agent");
+    }
+
+
+    public void http_lt1() {
+        System.out.println("测试结果：hi1");
+        http_lt2();
+    }
+
+    public void http_lt2() {
+        System.out.println("测试结果：hi2");
+        http_lt3();
+    }
+
+    public void http_lt3() {
+        System.out.println("测试结果：hi3");
     }
 }
