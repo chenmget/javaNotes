313428a4f75c5bfdffc01d46c5f130b0d9c8a1db
chenmin <chen.min55@iwhalecloud.com>
2020-09-16 18:55:12 +0800

    fix

---
+++ agent/src/main/java/com/agent/ApiTest.java
@@ -0,0 +1,13 @@
+package com.agent;
+
+/**
+ * @program: javaNotes
+ * @description:
+ * @author: cm
+ * @create: 2020-09-16 18:35
+ **/
+public class ApiTest {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
---
+++ agent/src/main/java/com/agent/MyAgent.java
@@ -0,0 +1,19 @@
+package com.agent;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * @program: javaNotes
+ * @description:
+ * @author: cm
+ * @create: 2020-09-16 18:30
+ **/
+public class MyAgent {
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("嗨！JavaAgent " + agentArgs);
+    }
+
+    //如果代理类没有实现上面的方法，那么 JVM 将尝试调用该方法
+    public static void premain(String agentArgs) {
+    }
+}
