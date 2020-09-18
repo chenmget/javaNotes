267c1e5ac66b23fa4dd7f68d67c31e9fda85ef4a
chenmget <502298798@qq.com>
2020-09-18 21:39:09 +0800

    fix

---
+++ netty/src/main/java/com/demo/springboot/NettyController.java
@@ -0,0 +1,26 @@
+package com.demo.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping(value = "/nettyserver", method = RequestMethod.GET)
+public class NettyController {
+
+    @Resource
+    private NettyServer nettyServer;
+
+    @RequestMapping("/localAddress")
+    public String localAddress() {
+        return "nettyServer localAddress " + nettyServer.getChannel().localAddress();
+    }
+
+    @RequestMapping("/isOpen")
+    public String isOpen() {
+        return "nettyServer isOpen " + nettyServer.getChannel().isOpen();
+    }
+
+}
