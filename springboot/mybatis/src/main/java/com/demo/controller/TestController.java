5763f00ad799acbd24abd533aef4599afa9d7265
chenmin <chen.min55@iwhalecloud.com>
2021-01-07 09:32:06 +0800

    fix

--- springboot/mybatis/src/main/java/com/demo/controller/TestController.java
+++ springboot/mybatis/src/main/java/com/demo/controller/TestController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/order")
-public class OrderController {
+public class TestController {
 
 
     @Autowired
